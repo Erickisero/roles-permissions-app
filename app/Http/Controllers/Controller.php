fa5184ad087ec0c197faf114baee5445e25e4061
Erick Kisero <ericsparta16@gmail.com>
2023-03-08 15:58:03 +0300

    role-permission

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+    
+namespace App\Http\Controllers;
+    
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as Basecontroller;
+
+class controller extends BaseController 
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
